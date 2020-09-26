df869af0ea53d191cb64e178c55a77e4923274a8
JigarShahBtpl <jigar.shah@botreetechnologies.com>
2020-09-26 13:54:53 +0530

    Added user model with devise integrations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { confirmations: 'devise/confirmations' }
+  get 'homes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
