27218d2d673d4514df24929d784717f05d992ab6
HarmishaPrajapati <harmisha.prajapati97@gmail.com>
2020-09-27 17:19:21 +0530

    Changed login, registration forms with theme forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def build_alert_classes(alert_type)
+    case alert_type.to_sym
+    when :notice, :success
+      'alert-success'
+    when :danger, :error, :alert
+      'alert-danger'
+    end
+  end
 end
