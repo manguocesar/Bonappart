73745b0d82c68684d3321b77dd4191d4e290d792
rathoddipak <dipak.rathod@botreetechnologies.com>
2020-09-27 12:47:18 +0530

    Added fields to user registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :firstname, :lastname, :birthdate, :address, :gender
+  validates :phone_no, :presence => true,
+                       :numericality => true,
+                       :length => { :minimum => 10, :maximum => 12 }
 end
