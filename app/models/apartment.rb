00f34a626815f14b7fc9b867a0ee128206483b13
dipakrathod <rathoddipak23797@gmail.com>
2020-09-29 23:43:45 +0530

    Added geocoder gem for geocoding...

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,4 +3,11 @@
 # Apartment model
 class Apartment < ApplicationRecord
   has_many_attached :images
+
+  geocoded_by :full_address
+  after_validation :geocode#, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+  def full_address
+    [country, city, area].compact.join(', ')
+  end
 end
