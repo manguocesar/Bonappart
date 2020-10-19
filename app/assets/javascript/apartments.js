4baa6c3fe79dc9e7614c4b6658ffffdebca9ef18
JigarShahBtpl <jigar.shah@botreetechnologies.com>
2020-10-19 21:34:23 +0530

    theme removed from application.

--- app/assets/javascript/apartments.js
+++ app/assets/javascript/apartments.js
@@ -2,11 +2,7 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap.min.js
-//= require moment
-//= require common/bootstrap-datetimepicker.min.js
-//= require common/bootstrap-select.min.js
 //= require jquery.validate
-//= require common/jquery.slimscroll.min
 //= require common/custom
 //= require login
 //= require apartments/custom
--- app/assets/javascript/payments.js
+++ app/assets/javascript/payments.js
@@ -2,6 +2,4 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap.min.js
-//= require common/bootstrap-select.min.js
-//= require common/jquery.slimscroll.min
 //= require common/custom
