1f8ad348a84af61d377c0769da6caeec899d0020
HarmishaPrajapati <harmisha.prajapati97@gmail.com>
2020-10-23 00:37:02 +0530

    Fix booking modal popup

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -21,7 +21,7 @@ class PaymentsController < ApplicationController
               end
     @payment.save!
     @payment.paid!
-    redirect_to apartments_path
+    redirect_to apartments_path, notice: t('apartment.booking_success')
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to add_payment_method_path
