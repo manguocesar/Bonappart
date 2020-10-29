22c45323db8fdc56bd4bfbf720a85b10c0c3ab2d
JigarShahBtpl <jigar.shah@botreetechnologies.com>
2020-10-29 17:58:53 +0530

    hotfix/ user.rb issue solved.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:login], :async
+         :recoverable, :rememberable, :validatable, :async, authentication_keys: [:login]
 
   has_one_attached :image
   has_many :apartments
