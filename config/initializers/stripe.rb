6de3cd9ae639213f77a5e569a08c9161061f702b
JaydipMakwana <jaydipmm97@gmail.com>
2020-11-28 16:41:26 +0530

    Put space after 'comma'

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  config.stripe.secret_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe,:secret_key)
-  config.stripe.publishable_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe,:publishable_key)
+  config.stripe.secret_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :secret_key)
+  config.stripe.publishable_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :publishable_key)
 end
