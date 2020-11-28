9210d2f20a549e8d5f4814772439712f61538506
JaydipMakwana <jaydipmm97@gmail.com>
2020-11-28 16:29:39 +0530

    Production Deployment Changes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  config.stripe.secret_key = 'sk_test_51HZY7lFYh30SOXdPVyejSzoOKDv71WbWEwgNYwE2VpaBEqT4ZwvjyV0L0lo4JGYhZrylQhX1Z3L2SbteVqfqBFsD00AI3c0Pdx'
-  config.stripe.publishable_key = 'pk_test_51HZY7lFYh30SOXdPiBXS9R16JA8sU4QpBTLzlWoSsnCRQhPc6NSRxsxDl047P6A71UWBSZcwduL3o5WQS6MvYAw000dcmzAW6p'
+  config.stripe.secret_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe,:secret_key)
+  config.stripe.publishable_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe,:publishable_key)
 end
