f8f8a20e0fca4ac256043450fb5ea6a44a937ea5
dipakrathod <rathoddipak23797@gmail.com>
2020-11-29 18:27:56 +0530

    Added booking creation admin side!

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -7,5 +7,15 @@ module Admin
     def index
       @bookings = pagination(Booking.all).per(12)
     end
+
+    def create
+      @booking = Booking.new(booking_params)
+      @booking.apartment = Apartment.find_by(id: params[:apartment])
+      if @booking.save
+        redirect_to admin_bookings_path
+      else
+        render :new
+      end
+    end
   end
 end
