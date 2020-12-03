2c7207f3207d91f220d7369036790e239093e9ef
JaydipMakwana <jaydipmm97@gmail.com>
2020-12-04 02:15:57 +0530

    Use constant value instead of string

--- app/helpers/admin/apartment_types_helper.rb
+++ app/helpers/admin/apartment_types_helper.rb
@@ -1,5 +1,5 @@
 module Admin::ApartmentTypesHelper
   def is_default_type?(apartment_type)
-    apartment_type&.name == 'default'
+    apartment_type&.name == Constant::DEFAULT_APARTMENT_TYPE
   end
 end
