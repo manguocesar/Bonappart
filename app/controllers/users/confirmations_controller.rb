20134eef0ca4463a25d7723185c1c2d4ea7e8fb2
JaydipMakwana <jaydipmm97@gmail.com>
2020-12-10 01:55:55 +0530

    en.yml issue resolved css issues resolved media query return

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -9,11 +9,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
     if resource.errors.empty?
-      set_flash_message(:notice, 'Your account is confirmed') if is_navigational_format?
+      set_flash_message!(:notice, :confirmed) if is_navigational_format?
       sign_in(resource_name, resource)
-      respond_with_navigational(resource){ redirect_to root_path }
+      respond_with_navigational(resource) { redirect_to root_path }
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
     end
   end
 end
