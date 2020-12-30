b8fd1ea476808520b50d6613288b34f2c95fe1f2
dipakrathod <rathoddipak23797@gmail.com>
2020-12-30 21:15:50 +0530

    Fixed feedback issues!

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -31,8 +31,8 @@ class PaymentsController < ApplicationController
     end
     if @payment&.id.present?
       redirect_to thank_you_payments_path
-      ConfirmationMailer.student_booking_confirmed_email(current_user&.id, find_landlord_user&.id).deliver_later
-      ConfirmationMailer.landlord_booking_confirmed_email(current_user&.id, find_landlord_user&.id).deliver_later
+      ConfirmationMailer.student_booking_confirmed_email(current_user&.id, find_landlord_user&.id, @payment.booked_apartment).deliver_later
+      ConfirmationMailer.landlord_booking_confirmed_email(current_user&.id, find_landlord_user&.id, @payment.booked_apartment).deliver_later
     else
       flash[:error] = @stripe_payment_record if @stripe_payment_record && @stripe_payment_record.kind_of?(String)
       redirect_to add_payment_method_path(amount: payment_params['amount'], booking_id: payment_params['booking_id'])
