379c0f2e3a97a584357775cd1a00e483e24ed2ae
dipakrathod <rathoddipak23797@gmail.com>
2020-12-30 21:16:23 +0530

    added apartment when sending an email

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -3,16 +3,18 @@
 # confirmation email
 class ConfirmationMailer < ApplicationMailer
   # Send confirmation email to student
-  def student_booking_confirmed_email(student_id, landlord_id)
+  def student_booking_confirmed_email(student_id, landlord_id, apartment)
     @student = find_user(student_id)
     @landlord = find_user(landlord_id)
+    @apartment = apartment
     mail(to: @student&.email, subject: 'Booking Confirmation')
   end
 
   # Send confirmation email to landlord
-  def landlord_booking_confirmed_email(student_id, landlord_id)
+  def landlord_booking_confirmed_email(student_id, landlord_id, apartment)
     @student = find_user(student_id)
     @landlord = find_user(landlord_id)
+    @apartment = apartment
     mail(to: @landlord&.email, subject: 'Booking Confirmation')
   end
 
