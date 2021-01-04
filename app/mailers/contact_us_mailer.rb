2f1c3bffbe692c9548e1fc4cd7717d9492c93b78
dipakrathod <rathoddipak23797@gmail.com>
2021-01-04 20:03:38 +0530

    Fixed live v1 fixes

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,12 +1,14 @@
 class ContactUsMailer < ApplicationMailer
-
   # Send contact us inquiry mail
   def send_inquiry(contact_us_id)
     return if contact_us_id.blank?
+
     contact_us_request = ContactUs.find_by(id: contact_us_id)
+    from_email = 'bonapartcontact@gmail.com'
+    to_email = contact_us_request&.email
     @sender_name = [contact_us_request&.first_name, contact_us_request&.last_name].join(' ')
     @message = contact_us_request&.message
     # Change Admin Email Here
-    mail(to: Rails.application.credentials.dig(Rails.env.to_sym, :admin_email), subject: contact_us_request&.subject) if contact_us_request&.email.present?
+    mail(to: from_email, from: to_email, subject: contact_us_request&.subject) if contact_us_request&.email.present?
   end
 end
